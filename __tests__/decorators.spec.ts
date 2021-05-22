a8df74acc7e1d5f34962c97f00667d31f6fa9ad8
fanyibin <f84041507@163.com>
2021-05-22 21:22:00 +0800

    test: Params decorator

--- __tests__/decorators.spec.ts
+++ __tests__/decorators.spec.ts
@@ -7,8 +7,13 @@ import {
   Response,
   setRequestConfig,
   Err,
+  Params,
+  createMethodDecorator,
 } from '../lib';
 
+type o = Record<string, any>;
+
+const Patch = createMethodDecorator('PATCH');
 const template = {
   errcode: 0,
   errmsg: 'ok',
@@ -19,33 +24,53 @@ Mock.mock('https://mock.api.com/get/list', 'get', template);
 Mock.mock('https://mock.api.com/post/list', 'post', template);
 Mock.mock('https://mock.api.com/put/list', 'put', template);
 Mock.mock('https://mock.api.com/delete/list', 'delete', template);
+Mock.mock('https://mock.api.com/patch/list', 'patch', template);
+Mock.mock(RegExp('https://mock.api.com/get-with-params.*'), 'get', (opt: o) => opt);
+Mock.mock(RegExp('https://mock.api.com/post-with-params.*'), 'post', (opt: o) => opt);
 
 setRequestConfig({ baseURL: 'https://mock.api.com' });
 
 class Request {
   @Get('/get/list')
-  fetchList(@Response res?: Record<string, any>, @Err err?: Error) {
+  fetchList(@Response res?: o, @Err err?: Error) {
     return [res, err];
   }
 
   @Post('/post/list')
-  postList(@Response res?: Record<string, any>) {
+  postList(@Response res?: o) {
     return res;
   }
 
   @Put('/put/list')
-  putList(@Response res?: Record<string, any>) {
+  putList(@Response res?: o) {
     return res;
   }
 
   @Delete('/delete/list')
-  deleteList(@Response res?: Record<string, any>) {
+  deleteList(@Response res?: o) {
+    return res;
+  }
+
+  @Patch('/patch/list')
+  patchList(@Response res?: o) {
     return res;
   }
 
   @Get('/error/api')
   fetchError(@Err err?: Error, @Response res?: unknown) {
     return [err, res];
+  }
+
+  @Get('/get-with-params')
+  @Params({ hello: 'world' })
+  getWithParams(@Params params: o, @Response res?: o) {
+    return res;
+  }
+
+  @Post('/post-with-params')
+  @Params({ hello: 'world' })
+  postWithParams(@Params params: o, @Response res?: o) {
+    return res;
   }
 }
 
@@ -61,24 +86,43 @@ describe('test decorators', () => {
   });
 
   it('test Post decorator', async () => {
-    const res: Record<string, any> | undefined = await request.postList();
+    const res: o | undefined = await request.postList();
     expect(res).toBeDefined();
     expect(res?.errcode).toBe(0);
     expect(res?.data.list).toContain('back end');
   });
 
   it('test Put decorator', async () => {
-    const res: Record<string, any> | undefined = await request.putList();
+    const res: o | undefined = await request.putList();
     expect(res).toBeDefined();
     expect(res?.errcode).toBe(0);
     expect(res?.data.list).toContain('back end');
   });
 
   it('test Delete decorator', async () => {
-    const res: Record<string, any> | undefined = await request.deleteList();
+    const res: o | undefined = await request.deleteList();
     expect(res).toBeDefined();
     expect(res?.errcode).toBe(0);
     expect(res?.data.list).toContain('back end');
+  });
+
+  it('test custom method decorator', async () => {
+    const res: o | undefined = await request.patchList();
+    expect(res).toBeDefined();
+    expect(res?.errcode).toBe(0);
+    expect(res?.data.list).toContain('back end');
+  });
+
+  it('test get api width params', async () => {
+    const res: o | undefined = await request.getWithParams({ foo: 'bar' });
+    expect(res?.url).toMatch('foo=bar');
+    expect(res?.url).toMatch('hello=world');
+  });
+
+  it('test post api width params', async () => {
+    const res: o | undefined = await request.postWithParams({ foo: 'bar' });
+    expect(res?.body).toMatch('"foo":"bar"');
+    expect(res?.body).toMatch('"hello":"world"');
   });
 
   // passed test
