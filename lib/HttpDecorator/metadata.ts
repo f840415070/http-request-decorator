453ecea3292e71186ed66c3d33ca01abcf502840
fanyibin <f84041507@163.com>
2021-05-26 16:55:08 +0800

    refactor: 使用枚举类型作为 metadata key

--- lib/HttpDecorator/metadata.ts
+++ lib/HttpDecorator/metadata.ts
@@ -1,46 +1,23 @@
-import { LooseObject } from './types';
+import { Metadata } from './types';
 
-const metaStore: Map<string, symbol> = new Map();
+const { getOwnMetadata } = Reflect;
 
-export const metakeyFor = (key: string) => {
-  if (!metaStore.has(key)) {
-    metaStore.set(key, Symbol(`meta:${key}`));
-  }
-  return metaStore.get(key);
-};
+export enum META {
+  HEADER,
+  PARAMS,
+  CONFIG,
+  PARAMS_INDEX,
+  CONFIG_INDEX,
+  RESPONSE_INDEX,
+  ERROR_INDEX,
+}
 
-export const HEADER = Symbol('meta:header');
-export const PARAMS = Symbol('meta:params');
-export const CONFIG = Symbol('meta:config');
-export const PARAMS_INDEX = Symbol('meta:params_index');
-export const CONFIG_INDEX = Symbol('meta:config_index');
-export const RESPONSE_INDEX = Symbol('meta:response_index');
-export const ERROR_INDEX = Symbol('meta:error_index');
-
-export const getAllMetadata = (target: Object, propertyKey: string | symbol) => {
-  const metadata: LooseObject = {};
-  for (const [key, metakey] of metaStore) {
-    metadata[key] = Reflect.getOwnMetadata(metakey, target, propertyKey);
-  }
-  return metadata;
-};
-
-// export const getAllMetadata = (target: Object, propertyKey: string | symbol) => {
-//   const { getOwnMetadata } = Reflect;
-//   const headers: Record<string, string | number> = getOwnMetadata(HEADER, target, propertyKey);
-//   const params: Record<string, any> = getOwnMetadata(PARAMS, target, propertyKey);
-//   const config: RequestConfig = getOwnMetadata(CONFIG, target, propertyKey);
-//   const paramsIndex: number = getOwnMetadata(PARAMS_INDEX, target, propertyKey);
-//   const configIndex: number = getOwnMetadata(CONFIG_INDEX, target, propertyKey);
-//   const responseIndex: number = getOwnMetadata(RESPONSE_INDEX, target, propertyKey);
-//   const errorIndex: number = getOwnMetadata(ERROR_INDEX, target, propertyKey);
-//   return {
-//     headers,
-//     config,
-//     params,
-//     configIndex,
-//     paramsIndex,
-//     responseIndex,
-//     errorIndex,
-//   };
-// };
+export const getAllMetadata = (target: Object, propertyKey: string | symbol): Metadata => ({
+  headers: getOwnMetadata(META.HEADER, target, propertyKey),
+  params: getOwnMetadata(META.PARAMS, target, propertyKey),
+  config: getOwnMetadata(META.CONFIG, target, propertyKey),
+  paramsIndex: getOwnMetadata(META.PARAMS_INDEX, target, propertyKey),
+  configIndex: getOwnMetadata(META.CONFIG_INDEX, target, propertyKey),
+  responseIndex: getOwnMetadata(META.RESPONSE_INDEX, target, propertyKey),
+  errorIndex: getOwnMetadata(META.ERROR_INDEX, target, propertyKey),
+});
