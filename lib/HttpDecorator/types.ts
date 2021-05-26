c1a9c53f9ccf711c0de9b0dff22d778bb6314650
fanyibin <f84041507@163.com>
2021-05-26 16:55:26 +0800

    feat: add types

--- lib/HttpDecorator/types.ts
+++ lib/HttpDecorator/types.ts
@@ -1,7 +1,21 @@
 import {
+  Method,
   AxiosRequestConfig,
+  AxiosResponse,
 } from 'axios';
 
 export type RequestConfig = AxiosRequestConfig;
+export type HttpMethod = Method;
+export type HttpResponse = AxiosResponse;
 
-export type LooseObject = Record<string, any>;
+export type LooseObject = Record<string | symbol, any>;
+
+export type Metadata = {
+  headers: LooseObject,
+  config: LooseObject,
+  params: RequestConfig,
+  configIndex: number,
+  paramsIndex: number,
+  responseIndex: number,
+  errorIndex: number,
+};
