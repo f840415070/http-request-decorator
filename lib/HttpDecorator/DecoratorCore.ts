3e20ef1c474a2705a85980a1ef1070fa9c56758d
fanyibin <f84041507@163.com>
2021-06-01 16:03:13 +0800

    refactor: 优化代码

--- lib/HttpDecorator/DecoratorCore.ts
+++ lib/HttpDecorator/DecoratorCore.ts
@@ -3,30 +3,34 @@ import {
   getDefaultRequestConfig,
   assignRequestConfig,
 } from './RequestConfig';
-import { isNumber, isObject } from './utils';
+import { isNumber } from './utils';
 import { getAllMetadata } from './metadata';
-import { HttpMethod, RequestConfig, Metadata } from './types';
+import {
+  HttpMethod,
+  RequestConfig,
+  Metadata,
+  HttpResponse,
+} from './types';
 
 const validIndex = (index: number) => isNumber(index) && index >= 0;
 
 const setConfig = (config: RequestConfig, metadata: Metadata, args: any[]) => {
-  if (validIndex(metadata.configIndex)) {
-    assignRequestConfig(config, args[metadata.configIndex]);
-  }
   if (metadata.config) {
     assignRequestConfig(config, metadata.config);
+  }
+  if (validIndex(metadata.configIndex)) {
+    assignRequestConfig(config, args[metadata.configIndex]);
   }
 };
 
 const setParams = (config: RequestConfig, metadata: Metadata, args: any[], method: HttpMethod) => {
-  const requestParams = {
-    ...metadata.params || {},
-    ...(
-      (validIndex(metadata.paramsIndex) && isObject(args[metadata.paramsIndex]))
-        ? args[metadata.paramsIndex]
-        : {}
-    ),
-  };
+  const requestParams = {};
+  if (metadata.params) {
+    Object.assign(requestParams, metadata.params);
+  }
+  if (validIndex(metadata.paramsIndex)) {
+    Object.assign(requestParams, args[metadata.paramsIndex]);
+  }
   // 'POST' | 'PUT' | 'PATCH' 请求的参数放在 data 字段，'GET' 等请求放在 params 字段
   const paramsField = (['POST', 'PUT', 'PATCH'].includes(method)) ? 'data' : 'params';
   assignRequestConfig(config, { [paramsField]: requestParams });
@@ -38,33 +42,35 @@ const setHeaders = (config: RequestConfig, metadata: Metadata) => {
   }
 };
 
+const configFromMetadata = (metadata: Metadata, url: string, method: HttpMethod, args: any[]): RequestConfig => {
+  const config: RequestConfig = getDefaultRequestConfig();
+  setConfig(config, metadata, args); // 设置请求配置
+  setParams(config, metadata, args, method); // 设置请求参数
+  setHeaders(config, metadata); // 设置 headers
+  config.url = url;
+  config.method = method;
+  return config;
+};
+
+const injectToArguments = <T = any>(args: any[], parameterIndex: number, value: T) => {
+  if (validIndex(parameterIndex)) {
+    args[parameterIndex] = value;
+  }
+};
+
 export function HttpMethodDecoratorFactory(method: HttpMethod, url: string) {
   return function (target: Object, propertyKey: string | symbol, descriptor: PropertyDescriptor) {
     const rawMethod: (...args: unknown[]) => void = descriptor.value;
-    descriptor.value = async function (...args: any[]) {
-      const requestConfig: RequestConfig = getDefaultRequestConfig();
+    descriptor.value = async function (...$args: any[]) {
       const metadata = getAllMetadata(target, propertyKey);
-
-      setConfig(requestConfig, metadata, args); // 设置请求配置
-      setParams(requestConfig, metadata, args, method); // 设置请求参数
-      setHeaders(requestConfig, metadata); // 设置 headers
-
-      requestConfig.url = url;
-      requestConfig.method = method;
-
       try {
+        const requestConfig = configFromMetadata(metadata, url, method, $args);
         const response = await axios(requestConfig);
-        if (validIndex(metadata.responseIndex)) {
-          args[metadata.responseIndex] = response;
-        }
-      } catch (e: unknown) {
-        // 错误捕获给 @Err 参数
-        if (validIndex(metadata.errorIndex)) {
-          args[metadata.errorIndex] = e;
-        }
+        injectToArguments<HttpResponse>($args, metadata.responseIndex, response);
+      } catch (exception: unknown) {
+        injectToArguments<unknown>($args, metadata.exceptionIndex, exception);
       }
-
-      return rawMethod.apply(this, args);
+      return rawMethod.apply(this, $args);
     };
   };
 }
