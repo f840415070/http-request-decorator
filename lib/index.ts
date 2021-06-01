e39646c6424bd75cf39dbc97124aad6d3d385722
fanyibin <f84041507@163.com>
2021-06-01 16:03:44 +0800

    chore: 导出类型文件

--- lib/index.ts
+++ lib/index.ts
@@ -3,4 +3,4 @@ import 'reflect-metadata';
 export * from './HttpDecorator/decorators';
 export { createMethodDecorator, interceptors } from './HttpDecorator/DecoratorCore';
 export { setRequestConfig } from './HttpDecorator/RequestConfig';
-export { AxiosRequestConfig, Method, AxiosResponse } from 'axios';
+export * from './HttpDecorator/types';
