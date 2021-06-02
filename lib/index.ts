11b9aa93e76f1152c267213bc79bd906f1bf809f
fanyibin <f84041507@163.com>
2021-06-02 22:08:00 +0800

    fix: 修正 createInstance 默认导出的表达式

--- lib/index.ts
+++ lib/index.ts
@@ -2,6 +2,6 @@ import 'reflect-metadata';
 
 export * from './HttpDecorator/decorators';
 export { interceptors } from './HttpDecorator/DecoratorCore';
-export * from './HttpDecorator/instance';
+export { default as createInstance } from './HttpDecorator/instance';
 export { requestConfig } from './HttpDecorator/RequestConfig';
 export * from './HttpDecorator/types';
