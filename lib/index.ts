4c845a67011a7bd65a473b8f2ac7df3398c438bf
fanyibin <f84041507@163.com>
2021-06-02 17:00:37 +0800

    feat: 导出 createInstance

--- lib/index.ts
+++ lib/index.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 
 export * from './HttpDecorator/decorators';
-export { createMethodDecorator, interceptors } from './HttpDecorator/DecoratorCore';
-export { setRequestConfig } from './HttpDecorator/RequestConfig';
+export { interceptors } from './HttpDecorator/DecoratorCore';
+export * from './HttpDecorator/instance';
+export { requestConfig } from './HttpDecorator/RequestConfig';
 export * from './HttpDecorator/types';
