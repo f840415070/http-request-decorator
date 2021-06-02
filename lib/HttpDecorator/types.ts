250637f6905f754762116cf91d73a1bb2f20ea88
fanyibin <f84041507@163.com>
2021-06-02 22:07:24 +0800

    fix: 修正 params 类型

--- lib/HttpDecorator/types.ts
+++ lib/HttpDecorator/types.ts
@@ -8,12 +8,12 @@ export type RequestConfig = AxiosRequestConfig;
 export type HttpMethod = Method;
 export type HttpResponse<T = any> = AxiosResponse<T>;
 
-export type LooseObject = Record<string, any>;
+export type LooseObject = Record<keyof any, any>;
 
 export type Metadata = {
   headers: LooseObject,
   config: LooseObject,
-  params: RequestConfig,
+  params: LooseObject,
   configIndex: number,
   paramsIndex: number,
   responseIndex: number,
